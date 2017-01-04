bffcf34afc238c4e229aa99deff72f6c9a42b106
asonas <hzw1258@gmail.com>
2017-01-04 17:35:37 +0900

    Update rec_radiko.sh
    https://gist.github.com/saiten/875864

--- rec_radiko.sh
+++ rec_radiko.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
-pid=$$
-date=`date '+%Y-%m-%d-%H:%M'`
-radiodate=`TZ=RST-4 date +%Y年%m月%d日`
-playerurl=http://radiko.jp/player/swf/player_3.0.0.01.swf
-playerfile="/tmp/player.swf"
-keyfile="/tmp/authkey.png"
+playerurl=http://radiko.jp/apps/js/flash/myplayer-release.swf
+cookiefile=./cookie.txt
+playerfile=./player.swf
+keyfile=./authkey.png
 
-if [ $# -eq 2 ]; then
+if [ $# -eq 1 ]; then
   channel=$1
-  DURATION=`expr $2 \* 60`
-  outdir="./"
-elif [ $# -eq 3 ]; then
+  output=./$1.flv
+elif [ $# -eq 2 ]; then
   channel=$1
-  DURATION=`expr $2 \* 60`
-  outdir=$3
+  output=$2
+elif [ $# -eq 4 ]; then
+  channel=$1
+  output=$2
+  mail=$3
+  pass=$4
 else
-  echo "usage : $0 channel_name duration(minuites) [output_basename]"
+  echo "usage : $0 channel_name [outputfile] [mail] [pass]"
   exit 1
+fi
+
+###
+# radiko premium
+###
+if [ $mail ]; then
+  wget -q --save-cookie=$cookiefile \
+       --keep-session-cookies \
+       --post-data="mail=$mail&pass=$pass" \
+       https://radiko.jp/ap/member/login/login
+
+  if [ ! -f $cookiefile ]; then
+    echo "failed login"
+    exit 1
+  fi
 fi
 
 #
@@ -36,7 +52,7 @@ fi
 # get keydata (need swftool)
 #
 if [ ! -f $keyfile ]; then
-  swfextract -b 14 $playerfile -o $keyfile
+  swfextract -b 12 $playerfile -o $keyfile
 
   if [ ! -f $keyfile ]; then
     echo "failed get keydata"
@@ -44,8 +60,8 @@ if [ ! -f $keyfile ]; then
   fi
 fi
 
-if [ -f auth1_fms_${pid} ]; then
-  rm -f auth1_fms_${pid}
+if [ -f auth1_fms ]; then
+  rm -f auth1_fms
 fi
 
 #
@@ -53,14 +69,14 @@ fi
 #
 wget -q \
      --header="pragma: no-cache" \
-     --header="X-Radiko-App: pc_1" \
-     --header="X-Radiko-App-Version: 2.0.1" \
+     --header="X-Radiko-App: pc_ts" \
+     --header="X-Radiko-App-Version: 4.0.0" \
      --header="X-Radiko-User: test-stream" \
      --header="X-Radiko-Device: pc" \
      --post-data='\r\n' \
      --no-check-certificate \
+     --load-cookies $cookiefile \
      --save-headers \
-     -O auth1_fms_${pid} \
      https://radiko.jp/v2/api/auth1_fms
 
 if [ $? -ne 0 ]; then
@@ -71,18 +87,18 @@ fi
 #
 # get partial key
 #
-authtoken=`perl -ne 'print $1 if(/x-radiko-authtoken: ([\w-]+)/i)' auth1_fms_${pid}`
-offset=`perl -ne 'print $1 if(/x-radiko-keyoffset: (\d+)/i)' auth1_fms_${pid}`
-length=`perl -ne 'print $1 if(/x-radiko-keylength: (\d+)/i)' auth1_fms_${pid}`
+authtoken=`perl -ne 'print $1 if(/x-radiko-authtoken: ([\w-]+)/i)' auth1_fms`
+offset=`perl -ne 'print $1 if(/x-radiko-keyoffset: (\d+)/i)' auth1_fms`
+length=`perl -ne 'print $1 if(/x-radiko-keylength: (\d+)/i)' auth1_fms`
 
 partialkey=`dd if=$keyfile bs=1 skip=${offset} count=${length} 2> /dev/null | base64`
 
 echo "authtoken: ${authtoken} \noffset: ${offset} length: ${length} \npartialkey: $partialkey"
 
-#rm -f auth1_fms_${pid}
+rm -f auth1_fms
 
-if [ -f auth2_fms_${pid} ]; then
-  rm -f auth2_fms_${pid}
+if [ -f auth2_fms ]; then
+  rm -f auth2_fms
 fi
 
 #
@@ -90,28 +106,28 @@ fi
 #
 wget -q \
      --header="pragma: no-cache" \
-     --header="X-Radiko-App: pc_1" \
-     --header="X-Radiko-App-Version: 2.0.1" \
+     --header="X-Radiko-App: pc_ts" \
+     --header="X-Radiko-App-Version: 4.0.0" \
      --header="X-Radiko-User: test-stream" \
      --header="X-Radiko-Device: pc" \
-     --header="X-Radiko-Authtoken: ${authtoken}" \
-     --header="X-Radiko-Partialkey: ${partialkey}" \
+     --header="X-Radiko-AuthToken: ${authtoken}" \
+     --header="X-Radiko-PartialKey: ${partialkey}" \
      --post-data='\r\n' \
+     --load-cookies $cookiefile \
      --no-check-certificate \
-     -O auth2_fms_${pid} \
      https://radiko.jp/v2/api/auth2_fms
 
-if [ $? -ne 0 -o ! -f auth2_fms_${pid} ]; then
+if [ $? -ne 0 -o ! -f auth2_fms ]; then
   echo "failed auth2 process"
   exit 1
 fi
 
 echo "authentication success"
 
-areaid=`perl -ne 'print $1 if(/^([^,]+),/i)' auth2_fms_${pid}`
+areaid=`perl -ne 'print $1 if(/^([^,]+),/i)' auth2_fms`
 echo "areaid: $areaid"
 
-rm -f auth2_fms_${pid}
+rm -f auth2_fms
 
 #
 # get stream-url
@@ -142,8 +158,5 @@ rtmpdump -v \
          -W $playerurl \
          -C S:"" -C S:"" -C S:"" -C S:$authtoken \
          --live \
-         --stop ${DURATION} \
-         --flv "/tmp/${channel}_${date}"
-
-ffmpeg -y -i "/tmp/${channel}_${date}" -acodec libmp3lame -ab 64k "${outdir}${radiodate}.mp3" && rm -f "/tmp/${channel}_${date}"
+         --flv $output
 ruby /home/asonas/app/radiko/podcast.rb
