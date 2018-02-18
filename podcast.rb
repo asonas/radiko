c3460dd090c2505bb574dd68358db7de892ba21d
asonas <hzw1258@gmail.com>
2018-02-18 18:21:34 +0900

    fixed path

--- podcast.rb
+++ podcast.rb
@@ -60,7 +60,7 @@ class Program
   end
 end
 
-YAML.load_file("programs.yml").each do |config|
+YAML.load_file(File.expand_path("programs.yml")).each do |config|
   config = Program.new(config)
   config.render
 end
