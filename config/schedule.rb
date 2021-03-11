1a32ace43e5c9cedc8ae61a4405a3b99be3659e0
Yuya Fujiwara <asonas@cookpad.com>
2021-03-12 01:34:47 +0900

    Update scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,13 +22,13 @@ set :output, "/home/asonas/app/radiko/log/cron.log"
 job_type :command, "cd :path && ':task' :output"
 
 every '0 3 * * 2'  do
-  command "CHANNEL=TBS RECORD_TIME=010000 RADIO_TITLE=JUNK伊集院光と深夜の馬鹿力 /usr/bin/env ruby recording.rb"
+  command "CHANNEL=TBS RECORD_TIME=0100 RADIO_TITLE=JUNK伊集院光と深夜の馬鹿力 ruby recording.rb"
 end
 
 every '0 1 * * 0'  do
-  command "CHANNEL=QRP RECORD_TIME=000000 RADIO_TITLE=上坂すみれの♡（はーと）をつければかわいかろう /usr/bin/env ruby recording.rb"
+  command "CHANNEL=QRR RECORD_TIME=0000 RADIO_TITLE=上坂すみれの♡（はーと）をつければかわいかろう ruby recording.rb"
 end
 
 every '30 12 * * 1-5'  do
-  command "CHANNEL=TBS RECORD_TIME=083000 RADIO_TITLE=伊集院光とらじおと /usr/bin/env ruby recording.rb"
+  command "CHANNEL=TBS RECORD_TIME=0830 RADIO_TITLE=伊集院光とらじおと ruby recording.rb"
 end
